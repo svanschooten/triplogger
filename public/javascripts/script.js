e4424a4c2df611c5d68d73b286bdb0d3fdb3d4ef
Stijn van Schooten <s.vanschooten@student.tudelft.nl>
2013-04-05 02:08:46 +0200

    some errors, but the canvas should be working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -83,30 +83,63 @@ function cancelRequest(cancelBuddy) {
 	});
 }
 
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 function setupHomeScreen() {
 	var counter = 0;
 	var canvasses = $(".tripCanvas");
-	var stepSize = canvasses.length;
-	//alert(stepSize);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	var stepSize = Math.floor(360 / canvasses.length + 1);
+	for (var i=0; i<canvasses.length; i++) {
+		var canvas = canvasses[i];
+		var ctx = canvas.getContext("2d");
+        var height = canvas.height;
+        var width = canvas.width;
+        var color = toRGB(counter, 1, 0.333);
+        ctx.fillStyle = rgbToHex(color[0], color[1], color[2]);
+        ctx.lineTo(0, 0);
+        ctx.lineTo(width, 0);
+        ctx.lineTo(width, height);
+        ctx.lineTo(0, height);
+        ctx.closePath();
+		ctx.fill();
+        for (var i2=0; i2<3; i2++) {
+			var x = Math.floor(Math.random() * width);
+			var y = Math.floor(Math.random() * height);
+            ctx.beginPath();
+			ctx.moveTo(width / 2,height / 2);
+			color = toRGB(counter, 1 - (Math.random() * 0.3), 0.333 + ((Math.random() * 0.2) - 0.1));
+			alert(rgbToHex(color[0], color[1], color[2]));
+			ctx.strokeStyle = rgbToHex(color[0], color[1], color[2]);
+            if(y < 150){
+            	ctx.lineTo(x, 0);
+                if(x < 150){
+                  ctx.lineTo(0,0);
+                  ctx.lineTo(0,y);
+                } else {
+                  ctx.lineTo(300,0);
+                  ctx.lineTo(300,y);
+                }
+              } else {
+				ctx.lineTo(x, 300);
+                if(x < 150){
+                  ctx.lineTo(0,300);
+                  ctx.lineTo(0,y);
+                } else {
+                  ctx.lineTo(300,300);
+                  ctx.lineTo(300,y);
+                }
+              }
+            ctx.lineTo(150,150);
+			ctx.closePath();
+			ctx.fill();
+        }
+        counter = counter + stepSize;
+	};
+};
